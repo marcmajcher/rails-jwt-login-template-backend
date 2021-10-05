c1cfd8b3893d12b5430a657b984eeacf2a6a3133
Marc Majcher <majcher@gmail.com>
2021-10-05 13:57:56 -0500

    notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  # This line is where you will include any method you want to allow to invoke without checking if it's authorized first.
+  # Up here is where you will include any method you want to allow the app to invoke 
+  # without checking if it's authorized first.
+
     skip_before_action :authorized, only: [:create]
    
     def create
